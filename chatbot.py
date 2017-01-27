f2537b4cea9f70d3db137015df48e0250784dba0
eneajaho <jahollarienea14@gmail.com>
2017-01-27 23:39:43 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,19 @@
 import sl4a
+import time
 droid = sl4a.Android()
 
+data1 = open("storage/sdcard1/ques.txt", 'r+')
+data2 = open("storage/sdcard1/ans.txt",'r+')
 
-data1 = open("questions.txt", 'r+')
-data2 = open("answers.txt",'r+')
-
-droid.ttsSpeak('Enter your name please >>')
+droid.ttsSpeak('Enter your name please')
+time.sleep(0.5)
 username = droid.recognizeSpeech()
 droid.setClipboard(username.result)
 uname = username.result
-
-droid.ttsSpeak("Hello" uname)
+droid.ttsSpeak("hello " + uname)
 
 def call():
+    time.sleep(1)
     inputer = droid.recognizeSpeech()
     droid.setClipboard(inputer.result)
     inputer1 = inputer.result    
@@ -20,20 +21,20 @@ def call():
         if inputer1+'\n'== i:
             index = predic.index(i)
             droid.ttsSpeak(answers [index])
-            kot = raw_input("Press anything to ask again >> ")
             call()
     call()
-    
-    
-al = 0
+        
+decide1=2
 i = 0
 predic = []
 answers = []
-if al == 0:
+if int(decide1)== 2:
     for u in data1:
         predic.append(u)
     for n in data2:
         answers.append(n)
+    droid.ttsSpeak("All data loaded" )
+    time.sleep(1)
     call()
 
 data1.close()
